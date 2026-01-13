8d13c42017203c60284c3d83facdf54b31417c1e
vohuynhduc11b2 <vohuynhduc11b2@gmail.com>
2026-01-13 18:50:29 +0700

    company list interface

---
+++ services/review/reviewApi.ts
@@ -0,0 +1,106 @@
+import { api } from '../api';
+import type {
+  AverageRatingsByCompanyResponse,
+  CalculateRecommendedPercentageByCompanyResponse,
+  CountAllReviewsResponse,
+  CountReviewsByCompanyResponse,
+  CreateReviewRequest,
+  CreateReviewResponse,
+  LatestReviewsResponse,
+  RatingSummaryByCompanyResponse,
+  ReviewsByCompanyRequest,
+  ReviewsByCompanyResponse,
+} from './reviewType';
+import type { CompanyIdRequest } from '../company/companyType';
+
+export const reviewApi = api.injectEndpoints({
+  endpoints: (builder) => ({
+    getAllReviewsByCompany: builder.query<ReviewsByCompanyResponse, ReviewsByCompanyRequest>({
+      query: (params) => {
+        const queryParams: any = {
+          page: params.page || 1,
+          size: params.size || 10,
+        };
+
+        return {
+          url: `/reviews/companies/${params.companyName}`,
+          method: 'GET',
+          params: queryParams,
+        };
+      },
+      providesTags: ['Review'],
+    }),
+
+    latestReviews: builder.query<LatestReviewsResponse, void>({
+      query: () => ({
+        url: `/reviews/latest`,
+        method: 'GET',
+      }),
+      providesTags: ['Review'],
+    }),
+
+    countReviewsByCompany: builder.query<CountReviewsByCompanyResponse, void>({
+      query: () => ({
+        url: `/reviews/companies/count`,
+        method: 'GET',
+      }),
+      providesTags: ['Review'],
+    }),
+
+    averageRatingsByCompany: builder.query<AverageRatingsByCompanyResponse, void>({
+      query: () => ({
+        url: `/reviews/companies/ratings/average`,
+        method: 'GET',
+      }),
+      providesTags: ['Review'],
+    }),
+
+    countAllReviews: builder.query<CountAllReviewsResponse, void>({
+      query: () => ({
+        url: `/reviews/count`,
+        method: 'GET',
+      }),
+      providesTags: ['Review'],
+    }),
+
+    getRatingByCompany: builder.query<RatingSummaryByCompanyResponse, CompanyIdRequest>({
+      query: (companyId) => ({
+        url: `/reviews/companies/${companyId}/ratings/summary`,
+        method: 'GET',
+      }),
+      providesTags: ['Review'],
+    }),
+
+    calculateRecommendedPercentageByCompany: builder.query<
+      CalculateRecommendedPercentageByCompanyResponse,
+      CompanyIdRequest
+    >({
+      query: (companyId) => ({
+        url: `/reviews/companies/${companyId}/recommendation-rate`,
+        method: 'GET',
+      }),
+      providesTags: ['Review'],
+    }),
+
+    createReview: builder.mutation<CreateReviewResponse, CreateReviewRequest>({
+      query: (body) => ({
+        url: `/reviews`,
+        method: 'POST',
+        data: body,
+      }),
+      invalidatesTags: ['User', 'Review', 'Company'],
+    }),
+  }),
+});
+
+export const {
+  useGetAllReviewsByCompanyQuery,
+  useLatestReviewsQuery,
+  useCountReviewsByCompanyQuery,
+  useAverageRatingsByCompanyQuery,
+  useCountAllReviewsQuery,
+  useGetRatingByCompanyQuery,
+  useCalculateRecommendedPercentageByCompanyQuery,
+  useCreateReviewMutation,
+} = reviewApi;
+
