1a8e7f1989028c1ac7c20c278fb3b8b5b1918a3f
Hachimandev <nguyentrungquyen2412@gmail.com>
2026-01-15 15:36:08 +0700

    feat: add blogTypes

--- app/(tabs)/blog.tsx
+++ app/(tabs)/blog.tsx
@@ -1,7 +1,59 @@
-import React from "react";
+import BlogCard from "@/components/blog/BlogCard";
+import { useGetBlogsQuery } from "@/services/blog/blogApi";
+import { useState } from "react";
+import {
+  ActivityIndicator,
+  FlatList,
+  RefreshControl,
+  View,
+} from "react-native";
 
-const blog = () => {
-  return <div></div>;
-};
+export default function BlogPage() {
+  const [page, setPage] = useState(0);
+  const size = 10;
 
-export default blog;
+  const { data, isLoading, isFetching, refetch } = useGetBlogsQuery({
+    page,
+    size,
+  });
+
+  const blogs = data?.items ?? [];
+
+  if (isLoading) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center" }}>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
+
+  return (
+    <FlatList
+      data={blogs}
+      keyExtractor={(item) => item.blogId.toString()}
+      renderItem={({ item }) => (
+        <BlogCard
+          blog={item}
+          onLike={() => {
+            // TODO: hook like
+          }}
+          onSave={() => {
+            // TODO: hook save
+          }}
+        />
+      )}
+      refreshControl={
+        <RefreshControl refreshing={isFetching} onRefresh={refetch} />
+      }
+      onEndReached={() => {
+        if (!isFetching && data && page + 1 < data.totalPages) {
+          setPage((prev) => prev + 1);
+        }
+      }}
+      onEndReachedThreshold={0.5}
+      ListFooterComponent={
+        isFetching ? <ActivityIndicator style={{ marginVertical: 16 }} /> : null
+      }
+    />
+  );
+}
---
+++ services/blog/blogType.ts
@@ -0,0 +1,52 @@
+import type { IBackendRes, IModelPaginate } from "@/types/api";
+import { BlogActionType } from "@/types/enum";
+import { Blog } from "@/types/model";
+
+export type BlogIdsRequest = {
+  blogIds: number[];
+  reason?: string;
+  mode: BlogActionType;
+};
+
+// Update Blog
+export type UpdateBlogRequest = {
+  blogId: number;
+  formData: FormData;
+};
+
+// Fetch with Pagination
+export type FetchBlogsRequest = {
+  page?: number;
+  size?: number;
+  sortBy?: string;
+  tags?: string[];
+  title?: string;
+  draft?: boolean;
+  enabled?: boolean;
+};
+
+export type FetchTagsRequest = {
+  keyword?: string;
+};
+
+// Response Types
+export type BlogMutationResponse = IBackendRes<Blog>;
+
+export type FetchBlogsResponse = IBackendRes<IModelPaginate<Blog>>;
+
+export type FetchBlogByIdResponse = IBackendRes<Blog>;
+
+export type FetchTagsResponse = IBackendRes<[[string, number]]>;
+
+export type BlogStatusResponse = IBackendRes<
+  {
+    blogId: number;
+    enabled: boolean;
+  }[]
+>;
+
+export type CreateCommentRequest = {
+  blogId: number;
+  comment: string;
+  replyTo?: number;
+};
