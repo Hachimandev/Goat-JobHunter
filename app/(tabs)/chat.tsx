5c9021653d76594c8d34b3b0d2aeec36aeeda6ed
Hachimandev <nguyentrungquyen2412@gmail.com>
2026-01-29 15:38:06 +0700

    feat: add search bar

--- app/(tabs)/chat.tsx
+++ app/(tabs)/chat.tsx
@@ -1,12 +1,30 @@
 import React from "react";
-import { Text } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const chat = () => {
   return (
-    <div>
-      <Text>Chat Screen</Text>
-    </div>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.searchBar}>
+        <Text style={styles.searchText}>🔍 Tìm kiếm</Text>
+      </View>
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#F4F4F6",
+  },
+
+  searchBar: {
+    padding: 12,
+    backgroundColor: "#0084FF",
+  },
+  searchText: {
+    color: "#fff",
+  },
+});
 
 export default chat;
