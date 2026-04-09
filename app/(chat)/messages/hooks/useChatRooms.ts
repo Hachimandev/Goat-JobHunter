79d7afd82af3f04567082fdac2397741268983e4
trgcghai <conghai.tpma@gmail.com>
2026-04-09 21:17:12 +0700

    feat(chat): enhance useChatRooms hook to conditionally fetch data based on user sign-in status

--- app/(chat)/messages/hooks/useChatRooms.ts
+++ app/(chat)/messages/hooks/useChatRooms.ts
@@ -4,15 +4,21 @@ import { ChatRoom } from '@/types/model';
 type UseChatRoomsOptions = {
   page?: number;
   size?: number;
+  isSignedIn?: boolean;
 };
 
 export function useChatRooms(options: UseChatRoomsOptions = {}) {
-  const { page = 1, size = 50 } = options;
+  const { page = 1, size = 50, isSignedIn } = options;
 
-  const { data, isLoading, isError, refetch } = useFetchChatRoomsQuery({
-    page,
-    size,
-  });
+  const { data, isLoading, isError, refetch } = useFetchChatRoomsQuery(
+    {
+      page,
+      size,
+    },
+    {
+      skip: !isSignedIn,
+    },
+  );
 
   const chatRooms: ChatRoom[] = data?.data?.result || [];
   const total = data?.data?.meta?.total || 0;
